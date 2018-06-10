87f62152815ecb5bb0bb60a896acba3de02fc391
Jiri Petvaldsky <jiripetvaldsky@Jiris-MacBook-Pro.local>
2018-06-10 16:09:06 +0200

    Initial Commit

---
+++ nette-app/test/html/index.php
@@ -0,0 +1,14 @@
+<?php 
+//phpinfo(); 
+
+echo __DIR__;
+
+
+$d = dir("../");
+echo "Handle: " . $d->handle . "\n";
+echo "Path: " . $d->path . "\n";
+while (false !== ($entry = $d->read())) {
+   echo $entry."\n";
+}
+$d->close();
+?>
